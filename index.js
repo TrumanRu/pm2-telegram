08a08e61a2528f5840e4337fbf3bf45eefb5f753
tsybanenko <cibanenko.i@oasiscatalog.com>
2022-04-08 21:36:55 +0300

    debug: Debug PM2 bus data

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const QUE_PROCESS_INTERVAL = 10000;
+
 const pm2 = require('pm2');
 const pmx = require('pmx');
 
@@ -13,24 +15,24 @@ const messages = [];
 
 let timer = null;
 
-function timeProcessor() {
+function queProcessor() {
     //console.log(new Date(), config.module_name);
     if (messages.length > 0) {
         console.log(messages);
         messages.length = 0;
     }
+    timer = setTimeout(queProcessor, QUE_PROCESS_INTERVAL);
 }
 
-function cbLogBus(data) {
+function cbBusLogger(data) {
     if (data.process.name !== config.module_name) {
         messages.push(data.data);
+        console.log(data);
     }
 }
 
 // Start listening on the PM2 BUS
 pm2.launchBus(function(err, bus) {
-
-    bus.on('log:err', cbLogBus);
-
-    timer = setInterval(timeProcessor, 10000);
+    bus.on('log:err', cbBusLogger);
+    timer = setTimeout(queProcessor, QUE_PROCESS_INTERVAL);
 });
