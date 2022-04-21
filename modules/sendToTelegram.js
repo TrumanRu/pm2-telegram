36af46da22f69ed97c39c69252f7a7293894f331
tsybanenko <cibanenko.i@oasiscatalog.com>
2022-04-22 02:47:25 +0300

    debug: Message replace characters #7

--- modules/sendToTelegram.js
+++ modules/sendToTelegram.js
@@ -7,7 +7,7 @@ const http = require('https');
  * @returns {Promise<unknown>}
  */
 module.exports = async function sendToTelegram(botToken, chatId, message) {
-  const postData = `chat_id=${chatId}&text=${message}`; //&parse_mode=Markdown
+  const postData = `chat_id=${chatId}&text=${encodeURIComponent(message)}`; //&parse_mode=Markdown
   /** @Type {https.RequestOptions} */
   const options = {
     hostname: 'api.telegram.org',
